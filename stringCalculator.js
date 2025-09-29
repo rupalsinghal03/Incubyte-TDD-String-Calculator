882bc262113ae5be92660077a61f63e4472967e7
rupal singhal <rupalsinghal03@gmail.com>
2025-09-29 14:14:01 +0530

    feat: support custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -7,7 +7,21 @@ function add(numbers) {
     // const nums = numbers.split(",").map(Number);
     // return nums.reduce((sum, n) => sum + n, 0);
 
-    const nums = numbers.split(/,|\n/).map(Number);
+    // const nums = numbers.split(/,|\n/).map(Number);
+    // return nums.reduce((sum, n) => sum + n, 0);
+
+    let delimiter = /,|\n/;
+
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        const delimiterLine = parts[0];
+        numbers = parts.slice(1).join("\n");
+
+        const customDelimiter = delimiterLine.slice(2).replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        delimiter = new RegExp(customDelimiter);
+    }
+
+    const nums = numbers.split(delimiter).map(Number);
     return nums.reduce((sum, n) => sum + n, 0);
 
 }
@@ -15,4 +29,5 @@ function add(numbers) {
 // console.log(add(""));
 // console.log(add("1"));
 // console.log(add("1,5,3"));
-console.log(add("1\n2,3"));
+// console.log(add("1\n2,3"));
+console.log(add("//;\n1;2")); 
