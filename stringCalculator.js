00586b9f3b76a22c1c77ada4a069887a219f68cd
rupal singhal <rupalsinghal03@gmail.com>
2025-09-29 14:16:18 +0530

    complete project

--- stringCalculator.js
+++ stringCalculator.js
@@ -22,10 +22,10 @@ function add(numbers) {
     }
 
     const nums = numbers.split(delimiter).map(Number);
-     const negatives = nums.filter(n => n < 0);
-  if (negatives.length > 0) {
-    throw new Error("negative numbers not allowed " + negatives.join(","));
-  }
+    const negatives = nums.filter(n => n < 0);
+    if (negatives.length > 0) {
+        throw new Error("negative numbers not allowed " + negatives.join(","));
+    }
     return nums.reduce((sum, n) => sum + n, 0);
 
 }
