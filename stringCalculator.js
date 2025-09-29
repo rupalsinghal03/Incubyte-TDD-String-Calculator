ad64b4043608370cd7c5d903d51cd5cedb789268
rupal singhal <rupalsinghal03@gmail.com>
2025-09-29 14:10:39 +0530

    feat: support sum of two comma-separated numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,14 @@
 function add(numbers) {
     if (!numbers) return 0;
 
-    const num = parseInt(numbers, 10);
-    return isNaN(num) ? 0 : num;
+    // const num = parseInt(numbers, 10);
+    // return isNaN(num) ? 0 : num;
+
+    const nums = numbers.split(",").map(Number);
+    return nums.reduce((sum, n) => sum + n, 0);
+
 }
 
 // console.log(add(""));
-console.log(add("1"));
+// console.log(add("1"));
+console.log(add("1,5"));
