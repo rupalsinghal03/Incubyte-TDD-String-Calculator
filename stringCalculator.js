69f34a7fca8b3acd9572dbe0e7f4987e1b9c4e8d
rupal singhal <rupalsinghal03@gmail.com>
2025-09-29 14:09:28 +0530

    feat: handle single number input

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,9 @@
-function add(numbers){
-    if(!numbers) return 0;
-    return 0;
+function add(numbers) {
+    if (!numbers) return 0;
 
+    const num = parseInt(numbers, 10);
+    return isNaN(num) ? 0 : num;
 }
 
-console.log(add(""));
+// console.log(add(""));
+console.log(add("1"));
